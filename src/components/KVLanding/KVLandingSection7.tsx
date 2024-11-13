f0a0def051116a7ee30ba4c4d3010daaf1a41839
nayounghye <yhna.dev@gmail.com>
2024-11-13 18:18:27 +0900

    Style: 섹션7 반응형 정상화

--- src/components/KVLanding/KVLandingSection7.tsx
+++ src/components/KVLanding/KVLandingSection7.tsx
@@ -1,24 +1,35 @@
-"use client";
-import React from "react";
-import "../../styles/globals.css";
-import "../../styles/Reset.css";
+'use client';
+import React from 'react';
+import '../../styles/globals.css';
+import '../../styles/Reset.css';
+import '../../styles/KVLanding/KVLandingSection7.css';
+import '../../styles/KV_landing_global.css';
 
 const KVLandingSection7 = () => {
-  const eventHandler = () => {
-    window.open("https://z17pcyw51c1.typeform.com/to/uTHb6raZ", "_blank");
+  const handleButtonClick = () => {
+    window.open('https://z17pcyw51c1.typeform.com/to/uTHb6raZ', '_blank');
   };
+
   return (
     <section className="kv-container">
       <article className="kv-seventh-mobile-box">
-        <img src="https://cdn.imweb.me/thumbnail/20241106/2bef43d560e10.png" />
+        <img
+          className="kv-seventh-img"
+          src="https://cdn.imweb.me/thumbnail/20241106/2bef43d560e10.png"
+          alt="맞춤형 솔루션 모바일 이미지"
+        />
+        <div className="kv-button-mobile" onClick={handleButtonClick}>
+          맞춤형 솔루션 받기
+        </div>
       </article>
       <article className="kv-seventh-desktop-box">
         <img
+          className="kv-seventh-img"
           src="https://cdn.imweb.me/thumbnail/20241106/b7f82040c8d41.png"
-          alt=""
+          alt="맞춤형 솔루션 데스크탑 이미지"
         />
       </article>
-      <div className="kv-button" onClick={eventHandler}>
+      <div className="kv-button" onClick={handleButtonClick}>
         맞춤형 솔루션 받기
       </div>
     </section>

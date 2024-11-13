fa2a1a77a2dc7f8aa81583200f5e0ee9643dbd75
aixion1506 <aixion1506@gmail.com>
2024-11-13 17:43:47 +0900

    Style: KI Section1 Migration

--- src/app/KoreaInvestment/page.tsx
+++ src/app/KoreaInvestment/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Section1 from "./components/Section1";
 import Section2 from "./components/Seciont2";
 
 const page = () => {
   return (
-    <div>
+    <div className="bg-black">
+      <Section1 />
       <Section2 />
     </div>
   );

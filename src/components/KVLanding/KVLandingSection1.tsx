08cb1edc79de85cefe15853b1feb6c863a07e5ea
nayounghye <yhna.dev@gmail.com>
2024-11-13 16:50:36 +0900

    Style : KV랜딩페이지 파일 이관 작업 및 섹션1 이관완료

---
+++ src/components/KVLanding/KVLandingSection1.tsx
@@ -0,0 +1,43 @@
+'use client';
+import React from 'react';
+import '../../styles/globals.css';
+import '../../styles/Reset.css';
+import '../../styles/KVLanding/KVLandingSection1.css';
+
+function KVLandingSection1() {
+  const openForm = () => {
+    window.open('https://z17pcyw51c1.typeform.com/to/hWFwHwCH', '_blank');
+  };
+
+  return (
+    <>
+      <div className="kv-first-container">
+        <div className="kv-slogan">
+          한국에서 활동을 시작하는
+          <br />
+          <span className="sloganText1">First Step</span>
+          <span className="sloganText2">Visa!</span>
+        </div>
+        <div className="kv-first-button" onClick={openForm}>
+          한국비자 신청하기
+        </div>
+      </div>
+
+      {/* Mobile Version -firstcontainer */}
+      <div className="kv-first-container-mobile">
+        <div className="kv-slogan-mobile">
+          <div className="sloganText-mobile">한국에서 활동을 시작하는</div>
+          <div className="sloganText2-mobile">
+            <span className="sloganText1">First Step</span>
+            <span>Visa!</span>
+          </div>
+        </div>
+        <div className="kv-first-button-mobile" onClick={openForm}>
+          <div className="text">한국비자 신청하기</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default KVLandingSection1;
---
+++ src/components/KVLanding/KVLandingSection7.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../../styles/globals.css';
+import '../../styles/Reset.css';
+
+const KVLandingSection7 = () => {
+  return <div>KVLandingSection7</div>;
+};
+
+export default KVLandingSection7;

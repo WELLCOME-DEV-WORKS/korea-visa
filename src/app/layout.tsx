a64a5b79b5580c529b952147c9c22398c9cc89a6
aixion1506 <aixion1506@gmail.com>
2024-11-14 16:17:04 +0900

    Feat: Google Analytics 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Header from "./ui/Header";
+import GoogleAnalytics from "@/utils/GoogleAnalytics";
 // import "../styles/globals.css";
 
 // const geistSans = localFont({
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
       // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics />
         <Header />
         {children}
       </body>

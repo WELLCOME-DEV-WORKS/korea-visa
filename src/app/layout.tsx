1d0b65debf1dfdaf7d5c60737b33c4f22c86c0c4
nayounghye <yhna.dev@gmail.com>
2024-11-14 16:26:28 +0900

    Style: header 디자인 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Header from "./ui/Header";
+import Header2 from "./ui/Header2";
 // import "../styles/globals.css";
 
 // const geistSans = localFont({
@@ -30,6 +31,7 @@ export default function RootLayout({
       // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
+        {/* <Header2 /> */}
         {children}
       </body>
     </html>

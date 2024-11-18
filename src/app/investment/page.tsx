ee001cb39ebe095ce8d4cbf949bdd00ea21de4e7
aixion1506 <aixion1506@gmail.com>
2024-11-18 14:58:30 +0900

    Refactor: investment 페이지 Migration 및 폴더 구조 개선

---
+++ src/app/investment/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Section1 from "@/pages/investment/components/Section1";
+import Section2 from "@/pages/investment/components/Seciont2";
+
+const page = () => {
+  return (
+    <div className="bg-black">
+      <Section1 />
+      <Section2 />
+    </div>
+  );
+};
+
+export default page;

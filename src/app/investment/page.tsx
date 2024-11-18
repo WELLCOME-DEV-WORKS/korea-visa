10aa218c7145863928e259ac2299b9bb1f5c949b
aixion1506 <aixion1506@gmail.com>
2024-11-18 15:43:16 +0900

    Remove: 8893a28 머지 커밋후 잔재 제거

--- src/app/investment/page.tsx
+++ src/app/investment/page.tsx
@@ -1,19 +1,12 @@
 import React from "react";
-<<<<<<< HEAD:src/app/investment/page.tsx
 import Section1 from "@/pages/investment/components/Section1";
 import Section2 from "@/pages/investment/components/Seciont2";
-=======
-import Section1 from "./components/Section1";
-import Section2 from "./components/Section2";
-import Section3 from "./components/Section3";
->>>>>>> 637292bbcad68fc2556509325dafc4e57876101b:src/app/Investment/page.tsx
 
 const page = () => {
   return (
     <div className="bg-[#0F0F0F]">
       <Section1 />
       <Section2 />
-      <Section3 />
     </div>
   );
 };
--- src/pages/investment/components/Section3.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Section3 = () => {
-  return <div className="container">Section3</div>;
-};
-
-export default Section3;

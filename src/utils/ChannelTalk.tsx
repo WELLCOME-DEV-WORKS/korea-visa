852fe981fb9fc4be3901e147288ef73909c6e844
nayounghye <144767162+nayounghye@users.noreply.github.com>
2024-11-19 10:54:04 +0900

    Hotfix: isChannelLoaded 변수 제거

--- src/utils/ChannelTalk.tsx
+++ src/utils/ChannelTalk.tsx
@@ -1,42 +1,55 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import * as ChannelService from "@channel.io/channel-web-sdk-loader";
 
-const ChannelTalk = () => {
-  const pluginKey = process.env.NEXT_PUBLIC_CHANNELTALK_PLUGIN_KEY;
-  const [isChannelLoaded, setIsChannelLoaded] = useState(false);
+interface ChannelTalkProps {
+  pluginKey: string | undefined;
+}
 
+// 채널톡 스크립트 로드 함수
+const loadChannelScript = async (pluginKey: string): Promise<void> => {
+  try {
+    await ChannelService.loadScript();
+    ChannelService.boot({
+      pluginKey,
+    });
+
+    // 채널톡 버튼 표시
+    ChannelService.showChannelButton();
+  } catch (err) {
+    console.error("ChannelTalk 스크립트 로드 실패:", err);
+  }
+};
+
+// 커스텀 훅
+const useChannelTalk = ({ pluginKey }: ChannelTalkProps): void => {
   useEffect(() => {
-    // 환경변수 검증
     if (!pluginKey) {
-      console.error("'NEXT_PUBLIC_CHANNELTALK_PLUGIN_KEY' 잘못된 듯 확인pls!");
+      console.error(
+        "환경변수 'NEXT_PUBLIC_CHANNELTALK_PLUGIN_KEY'가 정의되지 않았습니다."
+      );
       return;
     }
 
-    // 채널톡 초기화 함수
-    const resetChannelTalk = async () => {
-      try {
-        await ChannelService.loadScript();
-        ChannelService.boot({
-          pluginKey,
-        });
-        ChannelService.showChannelButton(); // 채널톡 기본 버튼 표시
-        setIsChannelLoaded(true);
-      } catch (err) {
-        console.error("ChannelTalk 초기화 실패:", err);
-      }
+    const initializeChannelTalk = async () => {
+      await loadChannelScript(pluginKey);
     };
 
-    resetChannelTalk();
+    initializeChannelTalk();
 
-    // 컴포넌트 언마운트 시 종료
     return () => {
       ChannelService.shutdown();
     };
   }, [pluginKey]);
+};
 
-  return <></>;
+// 메인 컴포넌트
+const ChannelTalk = () => {
+  const pluginKey = process.env.NEXT_PUBLIC_CHANNELTALK_PLUGIN_KEY;
+  useChannelTalk({ pluginKey });
+
+  return null;
 };
 
 export default ChannelTalk;

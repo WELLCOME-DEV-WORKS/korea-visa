57c1103a79f8804581471c312f56a69886275211
nayounghye <yhna.dev@gmail.com>
2024-11-19 11:20:31 +0900

    Style: 다크모드 제거

---
+++ public/fonts/index.ts
@@ -0,0 +1,19 @@
+import localFont from "next/font/local";
+
+export const gangwonEdu = localFont({
+  src: "/fonts/GangwonEduPower.otf",
+  display: "swap",
+  variable: "--font-gangwon",
+});
+
+export const suitVariable = localFont({
+  src: "/fonts/SUIT-Variable.woff2",
+  display: "swap",
+  variable: "--font-suit",
+});
+
+export const tenada = localFont({
+  src: "/fonts/Tenada.ttf",
+  display: "swap",
+  variable: "--font-tenada",
+});

a463eb2a7a3a5dd8e09da386b5620d211c63e529
aixion1506 <133094660+aixion1506@users.noreply.github.com>
2024-11-19 17:07:16 +0900

    MetaFixel, Google Tag 에러 수정 / koreavisa 랜딩페이지 마이그레이션 / 빌드 정상화 (#34)
    * Fix: noscript 태그의 HTML 인코딩으로 인한 하이드레이션 에러 수정,
    afterInteractive 옵션으로 방지
    
    * Recovery: koreavisa 공통 버튼 컴포넌트 복구
    
    * Feat: MetaFixel 외부 이미지 호스트  명시적 설정
    
    * Fix: GoogleTag SyntaxError - after argument list
    
    * Style: MetaPixel invisible
    
    * Migration: koreavisa 랜딩페이지
    
    * Rename: investment global css
    
    * Remove: 사용하지 않는 모듈로 인한 빌드오류 해결
    
    * Fix: CSS에서 '/' 문자로 인한 HookWebpackError 해결로 빌드 정상화

---
+++ src/pages/koreavisa/components/KVSection7.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import "../styles/KVSection7.css";
+
+const KVSection7 = () => {
+  const openLink = () => {
+    window.open("https://z17pcyw51c1.typeform.com/to/Pe8ZJkpG", "_blank");
+  };
+
+  return (
+    <section className="kv-container">
+      <article className="kv-seventh-mobile-box">
+        <img
+          className="kv-seventh-img"
+          src="https://cdn.imweb.me/thumbnail/20241106/2bef43d560e10.png"
+          alt="맞춤형 솔루션 모바일 이미지"
+        />
+        <div className="kv-button-mobile" onClick={openLink}>
+          맞춤형 솔루션 받기
+        </div>
+      </article>
+      <article className="kv-seventh-desktop-box">
+        <img
+          className="kv-seventh-img"
+          src="https://cdn.imweb.me/thumbnail/20241106/b7f82040c8d41.png"
+          alt="맞춤형 솔루션 데스크탑 이미지"
+        />
+      </article>
+      <div className="kv-button" onClick={openLink}>
+        맞춤형 솔루션 받기
+      </div>
+    </section>
+  );
+};
+
+export default KVSection7;
---
+++ src/pages/koreavisa/components/KVSection8.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import "../styles/KVSection8.css";
+import "../styles/KV_global.css";
+
+const KVSection8 = () => {
+  const openLink = () => {
+    window.open("https://z17pcyw51c1.typeform.com/to/Pe8ZJkpG", "_blank");
+  };
+
+  return (
+    <section className="kv-container">
+      <div className="kv-title-blue">WELL-COME을 선택한 이유?</div>
+      <article className="kv-eighth-mobile-box">
+        <img src="https://cdn.imweb.me/thumbnail/20241106/21d465269b4da.png" />
+      </article>
+      <article className="kv-eighth-desktop-box">
+        <img
+          src="https://cdn.imweb.me/thumbnail/20241106/56f6df93f28d2.png"
+          alt=""
+        />
+      </article>
+
+      <div className="kv-button" onClick={openLink}>
+        한국비자 신청하기
+      </div>
+      <div className="kv-button-mobile" onClick={openLink}>
+        한국비자 신청하기
+      </div>
+    </section>
+  );
+};
+
+export default KVSection8;

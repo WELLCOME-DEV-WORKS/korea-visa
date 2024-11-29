c29fa56f310d2e82881eb461329bccfaa569ef5a
nayounghye <yhna.dev@gmail.com>
2024-11-29 11:45:50 +0900

    Rename: user에서 usersignup으로 링크명 변경

---
+++ src/app/usersignin/page.tsx
@@ -0,0 +1,12 @@
+import UserSignIn from "@/pages/user/UserSignIn";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <UserSignIn />
+    </>
+  );
+};
+
+export default page;
